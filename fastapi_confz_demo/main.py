3679acd79e5004a28b0cfbe3c059432af5980d02
Silvan Melchior <silvan.melchior@zuehlke.com>
2021-11-12 10:29:00 +0100

    first simple app and db w/o any config

--- fastapi_confz_demo/main.py
+++ fastapi_confz_demo/main.py
@@ -1,8 +1,70 @@
-from fastapi import FastAPI
+from typing import Optional, List
 
-app = FastAPI()
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+
+app = FastAPI(title="My Application", version="0.0.1")
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class UserBase(SQLModel):
+    name: str
+
+
+class User(UserBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserRead(UserBase):
+    id: int
+
+
+sqlite_url = f"sqlite:///database.db"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.post("/user/", response_model=UserRead)
+def create_user(*, session: Session = Depends(get_session), user: UserCreate):
+    db_user = User.from_orm(user)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+
+@app.get("/user/", response_model=List[UserRead])
+def read_users(*, session: Session = Depends(get_session)):
+    users = session.exec(select(User)).all()
+    return users
